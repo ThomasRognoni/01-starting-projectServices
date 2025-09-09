cb462dd8d952ddb068f13beda852f4842c1394aa
ThomasRognoni <thomas.rognoni@beyondoc.com>
2025-09-09 14:23:34 +0200

    Injecting Other Values (NOT Services)

--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,11 @@ import { InjectionToken } from '@angular/core';
 import { AppComponent } from './app/app.component';
 import { TasksService } from './app/tasks/tasks.service';
 
-export const TasksServiceToken = new InjectionToken<TasksService>('tasks-service-token');
+export const TasksServiceToken = new InjectionToken<TasksService>(
+  'tasks-service-token'
+);
 
 bootstrapApplication(AppComponent, {
-  providers: [{provide: TasksServiceToken, useClass: TasksService}],
+  providers: [{ provide: TasksServiceToken, useClass: TasksService }],
 }).catch((err) => console.error(err)); //alternative with which we can provide the service
-// bootstrapApplication(AppComponent,).catch((err) => console.error(err));
+// bootstrapApplication(AppComponent).catch((err) => console.error(err));
