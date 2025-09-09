a7e7b044d6c49bd2827dea977b4f8d2703c20d43
ThomasRognoni <thomas.rognoni@beyondoc.com>
2025-09-09 12:21:36 +0200

    alternative to provide the services

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,9 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 
 import { AppComponent } from './app/app.component';
-import { TasksService } from './app/tasks/tasks.service';
+// import { TasksService } from './app/tasks/tasks.service';
 
-bootstrapApplication(AppComponent, {
-  providers: [TasksService],
-}).catch((err) => console.error(err));
+// bootstrapApplication(AppComponent, {
+//   providers: [TasksService],
+// }).catch((err) => console.error(err));   alternative whit which we can provide the service
+bootstrapApplication(AppComponent,).catch((err) => console.error(err));
