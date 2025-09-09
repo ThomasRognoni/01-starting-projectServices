ec84afda1056f7cd0cdee90b02e389ede34778d5
ThomasRognoni <thomas.rognoni@beyondoc.com>
2025-09-09 12:40:08 +0200

    alternatives for providing the service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 
 import { TasksComponent } from './tasks/tasks.component';
+import { LoggingService } from './logging.service';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html',
   imports: [TasksComponent],
+  // providers: [LoggingService], alternative way to provide the service
 })
 export class AppComponent {}
